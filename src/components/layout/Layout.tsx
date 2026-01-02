a31685dd6f06f6cab7500430b5de07c54d4afbd4
Tharun kamalesh <tharunkamalesh09@gmail.com>
2026-01-02 18:12:48 +0530

    Set entire website background to pure black with transparent sections

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -9,7 +9,7 @@ interface LayoutProps {
 
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="relative min-h-screen flex flex-col bg-[#0B0F14]">
+    <div className="relative min-h-screen flex flex-col bg-transparent">
       {/* VANTA.js background container is in index.html */}
       <Navbar className="relative z-20" />
       <main className="relative z-20 flex-1 pt-[73px]">{children}</main>
